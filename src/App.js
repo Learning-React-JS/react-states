09d43682f2dc307a96804aa5a06a7faed2828f29
ilkay Polat <ilkaypolat1985@gmail.com>
2021-12-22 21:41:51 +0100

    Added a new object into the component state

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ function App() {
   const [name, setName] = useState("Ilkay");
   const [age, setAge] = useState(34);
   const [friends, setFriends] = useState(["Ahmet", "Mehmet"]);
-
+  const [address, setAddress] = useState({ title: "Home", postCode: 34515 });
   console.log(name, age);
 
   return (
@@ -20,9 +20,19 @@ function App() {
       {friends.map((friend, key) => (
         <div key={key}>{friend}</div>
       ))}
-
       <button onClick={() => setFriends([...friends, "Fatma"])}>
         Add New Friend
+      </button>
+      <hr />
+      <br />
+      <br />
+      <h2>Address</h2>
+      <div>
+        {address.title} {address.postCode}
+      </div>
+      <br />
+      <button onClick={() => setAddress({ title: "Work", postCode: 4535 })}>
+        Change Adress
       </button>
     </div>
   );
